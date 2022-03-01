539d7eb7a191acdffe79022cd374bfab08202c47
Veeresh-Ranjan <99407299+Veeresh-Ranjan@users.noreply.github.com>
2022-03-02 00:44:12 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,32 @@
-#include <vector>
+#include<vector>
+#include<iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Stats
+{
+    public:
+    double min,max,average;
+    Stats()
+    {
+        //cout<<"Constructor"<<endl;
+    }
+
+    ~Stats()
+    {
+        //cout<<"Desc"<<endl;
+    }
+    double calculate_Avg(const std::vector<double>& b)
+    {
+        double sum = 0.0;
+        for(std::size_t i = 0; i < b.size(); i++)
+            sum += b.at(i);
+        return sum/b.size();
+    }
+};
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<double>& a);
 }
+
