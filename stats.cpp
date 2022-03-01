780005b34954860eccb936f44c45201d3535dc77
Veeresh-Ranjan <99407299+Veeresh-Ranjan@users.noreply.github.com>
2022-03-02 00:43:28 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,19 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+
+Stats Statistics::ComputeStatistics(const std::vector<double>& a) {
     //Implement statistics here
+    Stats stats_obj;
+    if(a.size()==0)
+    {
+        stats_obj.min=stats_obj.max=0.0/0.0;
+    }
+    else
+    {
+        stats_obj.min = *min_element(a.begin(), a.end());
+        stats_obj.max = *max_element(a.begin(), a.end());
+    }
+    stats_obj.average = stats_obj.calculate_Avg(a);
+    return stats_obj;
 }
+
